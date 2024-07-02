8c8677bbd36c63c51a7b5335fa7682893b12c59e
ahmedooo1 <aahmadooo997@gmail.com>
2024-07-02 17:32:43 +0200

    #

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-gray-800 p-4 text-white flex justify-between items-center">
-    <router-link to="/">
-      <img src="../assets/images/NF-EAT-transparent.png" alt="NF-EAT Logo" class="w-20">
+    <router-link to="/" @click.native="closeMenu">
+      <img src="../assets/images/NF-EAT-transparent.png" alt="NF-EAT Logo" class="w-20 logo">
     </router-link>
     <button @click="toggleMenu" class="md:hidden">
       <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/menu--v1.png" alt="menu" />
@@ -106,6 +106,9 @@ export default {
 </script>
 
 <style scoped>
+.logo{
+  z-index: 10;
+}
 @media (min-width: 768px) {
   .md\\:flex {
     display: flex !important;
