cd0b140963aafc7b7fe37d00f04e26c9769015d7
ahmedooo1 <aahmadooo997@gmail.com>
2024-07-05 12:31:45 +0200

    toast register updated

--- components/RegisterForm.vue
+++ components/RegisterForm.vue
@@ -54,7 +54,7 @@ export default {
         console.log('Payload:', payload)
         const response = await this.$axios.post('/user/register', payload)
         console.log('Registration successful:', response.data)
-        this.$toast.success('Registration successful:')
+        this.$toast.success('L\'inscription est réussi !')
         this.$emit('close')
       } catch (error) {
         console.error('Registration failed:', error.response ? error.response.data : error)
